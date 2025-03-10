04e86b8067a6f45503832c3adb4372eaaa9b534f
Bikram Baruah <baruahbikram05@gmail.com>
2025-03-10 15:35:41 +0000

    Use env vars

--- sql_handler.py
+++ sql_handler.py
@@ -1,18 +1,25 @@
 from sqlalchemy import create_engine, text
 import json
 from typing import Optional, Dict, Any
+import os
 
 class SQLQueryHandler:
     def __init__(self, config_path="db_config.json"):
-        self.engine = self._create_db_engine(config_path)
+        self.engine = self._create_db_engine()
         
-    def _create_db_engine(self, config_path):
-        # Load database configuration
-        with open(config_path, "r") as file:
-            config = json.load(file)
-            
+    def _create_db_engine(self):
+        # Load database configuration from environment variables
+        db_user = os.environ.get("DB_USER")
+        db_password = os.environ.get("DB_PASSWORD")
+        public_ip = os.environ.get("PUBLIC_IP")
+        port = os.environ.get("DB_PORT")
+        db_name = os.environ.get("DB_NAME")
+
+        # if not all([db_user, db_password, public_ip, port, db_name]):
+        #     raise ValueError("One or more required database environment variables are missing.")
+
         # Create connection string
-        connection_url = f"mysql+pymysql://{config['DB_USER']}:{config['DB_PASSWORD']}@{config['PUBLIC_IP']}:{config['PORT']}/{config['DB_NAME']}"
+        connection_url = f"mysql+pymysql://{db_user}:{db_password}@{public_ip}:{port}/{db_name}"
         return create_engine(connection_url)
     
     def execute_query(self, sql_query: str, params: Optional[Dict[str, Any]] = None, fetch: bool = True):
